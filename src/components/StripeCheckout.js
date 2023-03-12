3a92f81b7fd9484b6486333fe27c41e23a4c1508
Anurag Agarwal <anuragagarwal530@gmail.com>
2023-03-13 00:04:45 +0530

    removed local storage part 2

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,6 +1,15 @@
-import React from 'react'
-
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 const StripeCheckout = () => {
+  const navigate = useNavigate();
+  const {user} =useAuth0();
+  useEffect(()=>{
+     setInterval(()=>{
+      navigate('/');
+    },4000)
+
+  },[user])
   return (
     <div>
       <h4>payment will be added shortly <br />
